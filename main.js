3d601df25d7d944621bfb065a129586dde518415
Mohamed Saleh <mohamed20163858@gmail.com>
2025-03-15 01:15:47 +0200

    Intialize electron app

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // For simplicity; consider security implications in production
+      contextIsolation: false,
+    },
+  });
+  mainWindow.loadFile(path.join(__dirname, "src", "index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
